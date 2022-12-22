8bcbc3bdadd238901da54c2217c11c14595ee462
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-22 16:55:16 +0530

    "ADDED: Adding the file IO Services"

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -15,7 +15,7 @@ public class AddressBookMain {
 			System.out.println(AddressBook.AllAddresssBookLIst.keySet());
 
 			System.out.println(
-					"Select option\n1.Add Contact\n2.Edit Contact\n3.view Contacts\n4.Delete Contact\n5.Add New AddressBook\n6.Select AddressBook\n7.Search Contact\n8.Sort Contact\n9.Exit");
+					"Select option\n1.Add Contact\n2.Edit Contact\n3.view Contacts\n4.Delete Contact\n5.Add New AddressBook\n6.Select AddressBook\n7.Search Contact\n8.Sort Contact By Name Itself\n9. Add Multiple Contacts In AddressBook\n10.Sort The  Contacts BY Youurs Choice \n11.Read Data\n12.Write Data\n.13Exit");
 			int option = scanner.nextInt();
 			switch (option) {
 			case 1:
@@ -26,7 +26,7 @@ public class AddressBookMain {
 				addressBook.editContact();
 				break;
 			case 3:
-				addressBook.displayContact(addressBook.UsedAddressBook);
+				addressBook.displayContact(AddressBook.UsedAddressBook);
 				break;
 			case 4:
 				addressBook.deleteContact();
@@ -41,14 +41,24 @@ public class AddressBookMain {
 				addressBook.searchContact();
 				break;
 			case 8:
-				addressBook.sortContact();
+				addressBook.sortContactByName();
 				break;
 
 			case 9:
 				addressBook.addMultipleContacts();
 				break;
+
 			case 10:
-				addressBook.searchContact();
+				addressBook.sortContact1();
+				break;
+			case 11:
+				addressBook.readData();
+				break;
+			case 12:
+				addressBook.writeData();
+				break;
+			case 13:
+				addressBook.displayContact(addressBook.UsedAddressBook);
 				break;
 			default:
 				System.out.println(option + " Thats Not A Valid Option!!!");
